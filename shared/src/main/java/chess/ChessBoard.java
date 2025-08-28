e4eeea187448b4fd935c25711ea605d00ccbcbe3
Ben Hegewald <benhegewald@gmail.com>
2025-08-27 21:33:52 -0600

    ChessMove class looking good to go.

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -2,7 +2,9 @@ package chess;
 
 import java.util.Objects;
 
+import static chess.ChessGame.TeamColor.BLACK;
 import static chess.ChessGame.TeamColor.WHITE;
+import static chess.ChessPiece.PieceType.*;
 
 /**
  * A chessboard that can hold and rearrange chess pieces.
@@ -67,11 +69,11 @@ public class ChessBoard
         ChessBoard board = new ChessBoard();
 
         ChessPosition p1 = new ChessPosition(1,1);
-        ChessPiece pi1 = new ChessPiece(WHITE, ChessPiece.PieceType.ROOK);
+        ChessPiece pi1 = new ChessPiece(WHITE, ROOK);
         board.addPiece(p1, pi1);
 
         ChessPosition p2 = new ChessPosition(1,2);
-        ChessPiece pi2 = new ChessPiece(WHITE, ChessPiece.PieceType.KNIGHT);
+        ChessPiece pi2 = new ChessPiece(WHITE, KNIGHT);
         board.addPiece(p2, pi2);
 
         ChessPosition p3 = new ChessPosition(1,3);
@@ -91,13 +93,109 @@ public class ChessBoard
         board.addPiece(p6, pi6);
 
         ChessPosition p7 = new ChessPosition(1,7);
-        ChessPiece pi7 = new ChessPiece(WHITE, ChessPiece.PieceType.KNIGHT);
+        ChessPiece pi7 = new ChessPiece(WHITE, KNIGHT);
         board.addPiece(p7, pi7);
 
         ChessPosition p8 = new ChessPosition(1,8);
-        ChessPiece pi8 = new ChessPiece(WHITE, ChessPiece.PieceType.ROOK);
+        ChessPiece pi8 = new ChessPiece(WHITE, ROOK);
         board.addPiece(p8, pi8);
 
-        throw new RuntimeException("Not implemented");
+        ChessPosition p9 = new ChessPosition(2,1);
+        ChessPiece pi9 = new ChessPiece(WHITE, PAWN);
+        board.addPiece(p9, pi9);
+
+        ChessPosition p10 = new ChessPosition(2,2);
+        ChessPiece pi10 = new ChessPiece(WHITE, PAWN);
+        board.addPiece(p10, pi10);
+
+        ChessPosition p11 = new ChessPosition(2,3);
+        ChessPiece pi11 = new ChessPiece(WHITE, PAWN);
+        board.addPiece(p11, pi11);
+
+        ChessPosition p12 = new ChessPosition(2,4);
+        ChessPiece pi12 = new ChessPiece(WHITE, PAWN);
+        board.addPiece(p12, pi12);
+
+        ChessPosition p13 = new ChessPosition(2,5);
+        ChessPiece pi13 = new ChessPiece(WHITE, PAWN);
+        board.addPiece(p13, pi13);
+
+        ChessPosition p14 = new ChessPosition(2,6);
+        ChessPiece pi14 = new ChessPiece(WHITE, PAWN);
+        board.addPiece(p14, pi14);
+
+        ChessPosition p15 = new ChessPosition(2,7);
+        ChessPiece pi15 = new ChessPiece(WHITE, PAWN);
+        board.addPiece(p15, pi15);
+
+        ChessPosition p16 = new ChessPosition(2,8);
+        ChessPiece pi16 = new ChessPiece(WHITE, PAWN);
+        board.addPiece(p16, pi16);
+
+        ChessPosition p17 = new ChessPosition(7,1);
+        ChessPiece pi17 = new ChessPiece(BLACK, PAWN);
+        board.addPiece(p17, pi17);
+
+        ChessPosition p18 = new ChessPosition(7,2);
+        ChessPiece pi18 = new ChessPiece(BLACK, PAWN);
+        board.addPiece(p18, pi18);
+
+        ChessPosition p19 = new ChessPosition(7,3);
+        ChessPiece pi19 = new ChessPiece(BLACK, PAWN);
+        board.addPiece(p19, pi19);
+
+        ChessPosition p20 = new ChessPosition(7,4);
+        ChessPiece pi20 = new ChessPiece(BLACK, PAWN);
+        board.addPiece(p20, pi20);
+
+        ChessPosition p21 = new ChessPosition(7,5);
+        ChessPiece pi21 = new ChessPiece(BLACK, PAWN);
+        board.addPiece(p21, pi21);
+
+        ChessPosition p22 = new ChessPosition(7,6);
+        ChessPiece pi22 = new ChessPiece(BLACK, PAWN);
+        board.addPiece(p22, pi22);
+
+        ChessPosition p23 = new ChessPosition(7,7);
+        ChessPiece pi23 = new ChessPiece(BLACK, PAWN);
+        board.addPiece(p23, pi23);
+
+        ChessPosition p24 = new ChessPosition(7,8);
+        ChessPiece pi24 = new ChessPiece(BLACK, PAWN);
+        board.addPiece(p24, pi24);
+
+        ChessPosition p25 = new ChessPosition(8,1);
+        ChessPiece pi25 = new ChessPiece(BLACK, ROOK);
+        board.addPiece(p25, pi25);
+
+        ChessPosition p26 = new ChessPosition(8,2);
+        ChessPiece pi26 = new ChessPiece(BLACK, KNIGHT);
+        board.addPiece(p26, pi26);
+
+        ChessPosition p27 = new ChessPosition(8,3);
+        ChessPiece pi27 = new ChessPiece(BLACK, BISHOP);
+        board.addPiece(p27, pi27);
+
+        ChessPosition p28 = new ChessPosition(8,4);
+        ChessPiece pi28 = new ChessPiece(BLACK, QUEEN);
+        board.addPiece(p28, pi28);
+
+        ChessPosition p29 = new ChessPosition(8,5);
+        ChessPiece pi29 = new ChessPiece(BLACK, KING);
+        board.addPiece(p29, pi29);
+
+        ChessPosition p30 = new ChessPosition(8,6);
+        ChessPiece pi30 = new ChessPiece(BLACK, BISHOP);
+        board.addPiece(p30, pi30);
+
+        ChessPosition p31 = new ChessPosition(8,7);
+        ChessPiece pi31 = new ChessPiece(BLACK, KNIGHT);
+        board.addPiece(p31, pi31);
+
+        ChessPosition p32 = new ChessPosition(8,8);
+        ChessPiece pi32 = new ChessPiece(BLACK, ROOK);
+        board.addPiece(p32, pi32);
+
+        this.squares = board.squares;
     }
 }
--- shared/src/main/java/chess/ChessMove.java
+++ shared/src/main/java/chess/ChessMove.java
@@ -1,5 +1,7 @@
 package chess;
 
+import java.util.Objects;
+
 /**
  * Represents moving a chess piece on a chessboard
  * <p>
@@ -8,9 +10,36 @@ package chess;
  */
 public class ChessMove
 {
+    ChessPosition startPosition;
+    ChessPosition endPosition;
+    ChessPiece.PieceType promotionPiece;
 
     public ChessMove(ChessPosition startPosition, ChessPosition endPosition, ChessPiece.PieceType promotionPiece)
     {
+        this.startPosition = startPosition;
+        this.endPosition = endPosition;
+        this.promotionPiece = promotionPiece;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        ChessMove chessMove = (ChessMove) o;
+        return Objects.equals(startPosition, chessMove.startPosition) && Objects.equals(endPosition, chessMove.endPosition) && promotionPiece == chessMove.promotionPiece;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(startPosition, endPosition, promotionPiece);
+    }
+
+    @Override
+    public String toString() {
+        return "ChessMove{" +
+                "startPosition=" + startPosition +
+                ", endPosition=" + endPosition +
+                ", promotionPiece=" + promotionPiece +
+                '}';
     }
 
     /**
@@ -18,7 +47,7 @@ public class ChessMove
      */
     public ChessPosition getStartPosition()
     {
-        throw new RuntimeException("Not implemented");
+        return this.startPosition;
     }
 
     /**
@@ -26,7 +55,7 @@ public class ChessMove
      */
     public ChessPosition getEndPosition()
     {
-        throw new RuntimeException("Not implemented");
+        return this.endPosition;
     }
 
     /**
@@ -37,6 +66,6 @@ public class ChessMove
      */
     public ChessPiece.PieceType getPromotionPiece()
     {
-        throw new RuntimeException("Not implemented");
+        return this.promotionPiece;
     }
 }
