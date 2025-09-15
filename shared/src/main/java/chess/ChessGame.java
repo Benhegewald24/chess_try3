acb348177034c546ac73d22c61a64360911ec817
Ben Hegewald <benhegewald@gmail.com>
2025-09-14 18:09:01 -0600

    Will need TA help to reconfigure everything... but still working on Phase 1

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -61,6 +61,7 @@ public class ChessGame
      */
     public Collection<ChessMove> validMoves(ChessPosition startPosition)
     {
+        ChessPiece pi = new ChessPiece(board.getPiece(startPosition).pieceColor, board.getPiece(startPosition).getPieceType());
         throw new RuntimeException("Not implemented");
     }
 
