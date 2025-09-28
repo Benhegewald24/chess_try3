065ee61b54d73e9b4209c6a22455a6e8935d1458
Ben Hegewald <benhegewald@gmail.com>
2025-09-28 14:51:56 -0600

    I'm in over my head but just going to see how far I can get and made note of questions along the way.

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -2,6 +2,10 @@ package server;
 
 import io.javalin.*;
 
+import java.util.Dictionary;
+import java.util.Enumeration;
+import java.util.Scanner;
+
 public class Server
 {
 
@@ -16,12 +20,61 @@ public class Server
 
     public void registerUser()
     {
+        Scanner user_input = new Scanner(System.in);
+        Dictionary<String, String> dict = new Dictionary<String, String>()
+        {
+            @Override
+            public int size() {
+                return 0;
+            }
 
+            @Override
+            public boolean isEmpty() {
+                return false;
+            }
+
+            @Override
+            public Enumeration<String> keys() {
+                return null;
+            }
+
+            @Override
+            public Enumeration<String> elements() {
+                return null;
+            }
+
+            @Override
+            public String get(Object key) {
+                return "";
+            }
+
+            @Override
+            public String put(String key, String value) {
+                return "";
+            }
+
+            @Override
+            public String remove(Object key) {
+                return "";
+            }
+        };
+
+        System.out.println("Username: ");
+        String username = user_input.nextLine();
+        System.out.println("Password: ");
+        String password = user_input.nextLine();
+        System.out.println("Email: ");
+        String email = user_input.nextLine();
+        // format this data into Json
+        //send to Register handler (not sure where this is... reread instructions)
     }
 
     public void logInUser()
     {
-
+        // print to screen: "Username: "
+        // store user input
+        // print to screen: "Password: "
+        // password (initialized above) = userinput (whatever the user types)
     }
 
     public void logOutUser()
--- shared/src/main/java/model/UserData.java
+++ shared/src/main/java/model/UserData.java
@@ -5,15 +5,4 @@ public record UserData()
     static String username;
     static String password;
     static String email;
-
-//    public class registerUser()
-//    {
-        // print to screen: "Username: "
-        // username (initiatized above) = userinput (whatever the user types)
-        // print to screen: "Password: "
-        // password (initialized above) = userinput (whatever the user types)
-        // print to screen: "Email: "
-        //email (initialized above) = userinput (whatever the user types)
-        // format this data into Json
-//    }
 }
