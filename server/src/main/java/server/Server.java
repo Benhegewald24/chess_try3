7b63ba70d2d0aeb8e98deee96888c7f63843ab10
Ben Hegewald <benhegewald@gmail.com>
2025-10-01 18:51:09 -0600

    I think I made the connection between client and the server.

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -2,6 +2,7 @@ package server;
 
 import handler.*;
 import io.javalin.*;
+import io.javalin.http.Handler;
 
 import java.util.*;
 
@@ -12,12 +13,19 @@ public class Server //The Server should be serializing and deserializing!
 
     public Server()
     {
-        javalin = Javalin.create(config -> config.staticFiles.add("web"));
         // Ben's note: This is the constructor
         // Register your endpoints and exception handlers here.}
+        javalin = Javalin.create(config -> config.staticFiles.add("web"));
+        javalin.delete("/db", clearDB());
+        javalin.post("/user", registerUser());
+        javalin.post("/session", logInUser());
+        javalin.delete("/session", logOutUser());
+        javalin.get("/game", listGames());
+        javalin.post("game", createGame());
+        javalin.put("/game", joinGame());
     }
 
-    public void registerUser()
+    public Handler registerUser()
     {
         RegisterHandler rr = new RegisterHandler();
         Scanner user_input = new Scanner(System.in);
@@ -34,40 +42,47 @@ public class Server //The Server should be serializing and deserializing!
         my_map.put("email", email);
 
         rr.register_h(my_map); //send map to Register handler (RegisterHandler)
+        return null;
     }
 
-    public void logInUser()
+    public Handler logInUser()
     {
         // print to screen: "Username: "
         // store user input
         // print to screen: "Password: "
         // password (initialized above) = userinput (whatever the user types)
+        return null;
     }
 
-    public void logOutUser()
+    public Handler logOutUser()
     {
 
+        return null;
     }
 
-    public void listGames()
+    public Handler listGames()
     {
 
+        return null;
     }
 
-    public void createGame()
+    public Handler createGame()
     {
 
+        return null;
     }
 
-    public void joinGame()
+    public Handler joinGame()
     {
 
+        return null;
     }
 
-    public void clearDB()
+    public Handler clearDB()
     {
         ClearGameHandler clear_game_request = new ClearGameHandler();
         clear_game_request.clear(clear_game_request);
+        return null;
     }
 
 
