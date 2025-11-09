c4eba021a20cc36e369325e02fc1ccd6219cf9f5
Ben Hegewald <benhegewald@gmail.com>
2025-11-08 22:55:58 -0700

    Getting started - Phase 5

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -4,26 +4,29 @@ import org.junit.jupiter.api.*;
 import server.Server;
 
 
-public class ServerFacadeTests {
+public class ServerFacadeTests
+{
 
     private static Server server;
 
     @BeforeAll
-    public static void init() {
+    public static void init()
+    {
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
     }
 
     @AfterAll
-    static void stopServer() {
+    static void stopServer()
+    {
         server.stop();
     }
 
 
     @Test
-    public void sampleTest() {
+    public void sampleTest()
+    {
         Assertions.assertTrue(true);
     }
-
 }
