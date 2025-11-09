3d35212cdc64ada7d2055a9c3846b821a4e6e5e1
Ben Hegewald <benhegewald@gmail.com>
2025-11-09 14:32:39 -0700

    Adding instructions for faster viewing

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,10 +1,16 @@
 import chess.*;
+import requests.LoginRequest;
+import requests.RegisterRequest;
 import server.Server;
 
 import java.util.Scanner;
 
+import static java.lang.Boolean.FALSE;
+import static java.lang.Boolean.TRUE;
+
 public class Main
 {
+    static boolean logged_in = FALSE;
     public static void main(String[] args)
     {
         System.out.println("Welcome to 240 chess. Type Help to get started.");
@@ -37,11 +43,15 @@ public class Main
             System.out.println("Password: ");
             String password = scanner.nextLine();
             Server server = new Server();
+            LoginRequest request = new LoginRequest(username, password);
+
+            logged_in = TRUE;
             server.loginHandler(context);
 
             //Prompts the user to input login information. Calls the server login API to login the user.
             // When successfully logged in, the client should transition to the Postlogin UI.
         }
+
         if (user_input.equalsIgnoreCase("Register") || user_input.equalsIgnoreCase("R"))
         {
             System.out.println("Username: ");
@@ -50,6 +60,10 @@ public class Main
             String password = scanner.nextLine();
             System.out.println("Email: ");
             String email = scanner.nextLine();
+            RegisterRequest request = new RegisterRequest(username, password, email);
+            logged_in = TRUE;
+
+            server.registerHandler(request);
             //. Calls the server register API to register and login the user. If successfully registered,
             // the client should be logged in and transition to the Postlogin UI.
 
