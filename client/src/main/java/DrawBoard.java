8d0e5ce7a0d8efe668cc44d75ac37447feda106d
Ben Hegewald <benhegewald@gmail.com>
2025-11-09 22:03:08 -0700

    Trying to think through how to make my displayboard dynamic... seems difficult. Need to look at phase 6 and work backwords?

--- client/src/main/java/DrawBoard.java
+++ client/src/main/java/DrawBoard.java
@@ -1,17 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+
 import static ui.EscapeSequences.*;
 
 public class DrawBoard
 {
+    private final String GREEN_COLOR = "\u001b[48;2;122;148;90m";
+    private final String TAN_COLOR = "\u001b[48;2;221;225;196m";
+    private final String DARK_GREY_COLOR = "\u001b[48;2;41;45;57m";
+
+    //hard-coded solution
+//    public void displayBoard()
+//    {
+//        System.out.println("8 " + GREEN_COLOR + BLACK_ROOK + TAN_COLOR + BLACK_KNIGHT + GREEN_COLOR + BLACK_BISHOP + TAN_COLOR + BLACK_QUEEN + GREEN_COLOR + BLACK_KING + TAN_COLOR + BLACK_BISHOP + GREEN_COLOR + BLACK_KNIGHT + TAN_COLOR + BLACK_ROOK + DARK_GREY_COLOR);
+//        System.out.println("7 " + TAN_COLOR + BLACK_PAWN + GREEN_COLOR + BLACK_PAWN + TAN_COLOR + BLACK_PAWN + GREEN_COLOR + BLACK_PAWN + TAN_COLOR + BLACK_PAWN + GREEN_COLOR + BLACK_PAWN + TAN_COLOR + BLACK_PAWN + GREEN_COLOR + BLACK_PAWN + DARK_GREY_COLOR);
+//        System.out.println("6 " + GREEN_COLOR + EMPTY + TAN_COLOR + EMPTY + GREEN_COLOR + EMPTY + TAN_COLOR + EMPTY + GREEN_COLOR + EMPTY + TAN_COLOR + EMPTY + GREEN_COLOR + EMPTY + TAN_COLOR + EMPTY + DARK_GREY_COLOR);
+//        System.out.println("5 " + TAN_COLOR + EMPTY + GREEN_COLOR + EMPTY + TAN_COLOR + EMPTY + GREEN_COLOR + EMPTY + TAN_COLOR + EMPTY + GREEN_COLOR + EMPTY + TAN_COLOR + EMPTY + GREEN_COLOR + EMPTY + DARK_GREY_COLOR);
+//        System.out.println("4 " + GREEN_COLOR + EMPTY + TAN_COLOR + EMPTY + GREEN_COLOR + EMPTY + TAN_COLOR + EMPTY + GREEN_COLOR + EMPTY + TAN_COLOR + EMPTY + GREEN_COLOR + EMPTY + TAN_COLOR + EMPTY + DARK_GREY_COLOR);
+//        System.out.println("3 " + TAN_COLOR + EMPTY + GREEN_COLOR + EMPTY + TAN_COLOR + EMPTY + GREEN_COLOR + EMPTY + TAN_COLOR + EMPTY + GREEN_COLOR + EMPTY + TAN_COLOR + EMPTY + GREEN_COLOR + EMPTY + DARK_GREY_COLOR);
+//        System.out.println("2 " + GREEN_COLOR + WHITE_PAWN + TAN_COLOR + WHITE_PAWN + GREEN_COLOR + WHITE_PAWN + TAN_COLOR + WHITE_PAWN + GREEN_COLOR + WHITE_PAWN + TAN_COLOR + WHITE_PAWN + GREEN_COLOR + WHITE_PAWN + TAN_COLOR + WHITE_PAWN + DARK_GREY_COLOR);
+//        System.out.println("1 " + TAN_COLOR + WHITE_ROOK + GREEN_COLOR + WHITE_KNIGHT + TAN_COLOR + WHITE_BISHOP + GREEN_COLOR + WHITE_QUEEN + TAN_COLOR + WHITE_KING + GREEN_COLOR + WHITE_BISHOP + TAN_COLOR + WHITE_KNIGHT + GREEN_COLOR + WHITE_ROOK + DARK_GREY_COLOR);
+//        System.out.println("   a  b  c  d  e  f  g  h ");
+//    }
     public void displayBoard()
     {
-        System.out.println("\u001b[31;44;1m 8" + BLACK_ROOK + BLACK_KNIGHT + BLACK_BISHOP + BLACK_QUEEN + BLACK_KING + BLACK_BISHOP + BLACK_KNIGHT + BLACK_ROOK);
-        System.out.println("\u001b[31;44;1m 7" + BLACK_PAWN + BLACK_PAWN + BLACK_PAWN + BLACK_PAWN + BLACK_PAWN + BLACK_PAWN + BLACK_PAWN + BLACK_PAWN);
-        System.out.println("\u001b[31;44;1m 6" + EMPTY + EMPTY + EMPTY + EMPTY + EMPTY + EMPTY + EMPTY + EMPTY);
-        System.out.println("\u001b[31;44;1m 5" + EMPTY + EMPTY + EMPTY + EMPTY + EMPTY + EMPTY + EMPTY + EMPTY);
-        System.out.println("\u001b[31;44;1m 4" + EMPTY + EMPTY + EMPTY + EMPTY + EMPTY + EMPTY + EMPTY + EMPTY);
-        System.out.println("\u001b[31;44;1m 3" + EMPTY + EMPTY + EMPTY + EMPTY + EMPTY + EMPTY + EMPTY + EMPTY);
-        System.out.println("\u001b[31;44;1m 2" + WHITE_PAWN + WHITE_PAWN + WHITE_PAWN + WHITE_PAWN + WHITE_PAWN + WHITE_PAWN + WHITE_PAWN + WHITE_PAWN);
-        System.out.println("\u001b[31;44;1m 1" + WHITE_ROOK + WHITE_KNIGHT + WHITE_BISHOP + WHITE_QUEEN + WHITE_KING + WHITE_BISHOP + WHITE_KNIGHT + WHITE_ROOK);
-        System.out.println("\u001b[31;44;1m   a  b  c  d  e  f  g  h ");
+        for (int i = 8; i > 0; i--)
+        {
+            System.out.println(i);
+            System.out.print(TAN_COLOR + BLACK_ROOK);
+        }
+
+        String letters = "  a  b  c  d  e  f  g  h";
+        System.out.println(letters);
     }
 }
--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,8 +1,6 @@
-import java.io.IOException;
 import java.util.Scanner;
+
 import results.LoginResult;
-import static java.lang.Boolean.FALSE;
-import static java.lang.Boolean.TRUE;
 
 public class Main
 {
@@ -15,113 +13,162 @@ public class Main
     public static void main(String[] args)
     {
         System.out.println("Welcome to 240 chess. Type [H]elp to get started.");
+        DrawBoard d = new DrawBoard();
+        d.displayBoard();
 
-        while (loggedIn == false)
-        {
-            String userInput = SCANNER.nextLine().trim();
-
-            if (userInput.equalsIgnoreCase("help") || userInput.equalsIgnoreCase("h"))
-            {
-                help();
-            }
-
-            else if (userInput.equalsIgnoreCase("log in") || userInput.equalsIgnoreCase("l"))
-            {
-                login();
-            }
-
-            else if (userInput.equalsIgnoreCase("register") || userInput.equalsIgnoreCase("r"))
-            {
-                register();
-            }
-
-            else if (userInput.equalsIgnoreCase("quit") || userInput.equalsIgnoreCase("q"))
-            {
-                System.out.println("Thanks for playing!");
-                System.exit(0);
-            }
-
-            else
-            {
-                System.out.println("Invalid input.");
-            }
-        }
-
-        loggedInUI();
-    }
-
-    public static void help()
-    {
-        System.out.println("What would you like to do?");
-        System.out.println("1. [L]og In");
-        System.out.println("2. [R]egister");
-        System.out.println("3. [Q]uit");
-    }
-
-    private static void login()
-    {
-        System.out.print("Username: ");
-        var username = SCANNER.nextLine().trim();
-
-        System.out.print("Password: ");
-        var password = SCANNER.nextLine();
-
-        try
-        {
-            activeSession = SERVER_FACADE.login(username, password);
-            loggedIn = true;
-            System.out.println("Welcome back, " + username + "!");
-        }
-        catch (Exception ex)
-        {
-            System.out.println("Login failed");
-        }
-    }
-
-    private static void register()
-    {
-        System.out.print("Username: ");
-        var username = SCANNER.nextLine().trim();
-
-        System.out.print("Password: ");
-        var password = SCANNER.nextLine();
-
-        System.out.print("Email: ");
-        var email = SCANNER.nextLine();
-
-        try
-        {
-            activeSession = SERVER_FACADE.register(username, password, email);
-            loggedIn = true;
-            System.out.println("Welcome to chess 240, " + username + "!");
-        }
-        catch (Exception ex)
-        {
-            System.out.println("Registrationfailed");
-        }
-    }
-
-      private static void loggedInUI()
-    {
-        while (loggedIn)
-        {
-            System.out.println("You are now logged in.\nType [H]elp for options");
-            String userInput = SCANNER.nextLine().trim();
-
-            if (userInput.equalsIgnoreCase("help") || userInput.equalsIgnoreCase("h"))
-            {
-                help_logged_in();
-            }
-        }
-    }
-
-    public static void help_logged_in()
-    {
-        System.out.println("What would you like to do?");
-        System.out.println("1. [L]og Out");
-        System.out.println("2. [C]reate Game");
-        System.out.println("3. [Li]st Games");
-        System.out.println("4. [P]lay Game");
-        System.out.println("5. [L]og Out");
+//
+//        while (loggedIn == false)
+//        {
+//            String userInput = SCANNER.nextLine().trim();
+//
+//            if (userInput.equalsIgnoreCase("help") || userInput.equalsIgnoreCase("h"))
+//            {
+//                help();
+//            }
+//
+//            else if (userInput.equalsIgnoreCase("log in") || userInput.equalsIgnoreCase("l"))
+//            {
+//                login();
+//            }
+//
+//            else if (userInput.equalsIgnoreCase("register") || userInput.equalsIgnoreCase("r"))
+//            {
+//                register();
+//            }
+//
+//            else if (userInput.equalsIgnoreCase("quit") || userInput.equalsIgnoreCase("q"))
+//            {
+//                System.out.println("Thanks for playing!");
+//                System.exit(0);
+//            }
+//
+//            else
+//            {
+//                System.out.println("Invalid input.");
+//            }
+//        }
+//
+//        loggedInUI();
+//    }
+//
+//    public static void help()
+//    {
+//        System.out.println("What would you like to do?");
+//        System.out.println("1. [L]og In");
+//        System.out.println("2. [R]egister");
+//        System.out.println("3. [Q]uit");
+//    }
+//
+//    private static void login()
+//    {
+//        System.out.print("Username: ");
+//        var username = SCANNER.nextLine().trim();
+//
+//        System.out.print("Password: ");
+//        var password = SCANNER.nextLine();
+//
+//        try
+//        {
+//            activeSession = SERVER_FACADE.login(username, password);
+//            loggedIn = true;
+//            System.out.println("Welcome back, " + username + "!");
+//        }
+//        catch (Exception ex)
+//        {
+//            System.out.println("Login failed");
+//        }
+//    }
+//
+//    private static void register()
+//    {
+//        System.out.print("Username: ");
+//        var username = SCANNER.nextLine().trim();
+//
+//        System.out.print("Password: ");
+//        var password = SCANNER.nextLine();
+//
+//        System.out.print("Email: ");
+//        var email = SCANNER.nextLine();
+//
+//        try
+//        {
+//            activeSession = SERVER_FACADE.register(username, password, email);
+//            loggedIn = true;
+//            System.out.println("Welcome to chess 240, " + username + "!");
+//        }
+//        catch (Exception ex)
+//        {
+//            System.out.println("Registrationfailed");
+//        }
+//    }
+//
+//      private static void loggedInUI()
+//    {
+//        while (loggedIn)
+//        {
+//            System.out.println("You are now logged in.\nType [H]elp for options");
+//            String userInput = SCANNER.nextLine().trim();
+//
+//            if (userInput.equalsIgnoreCase("help") || userInput.equalsIgnoreCase("h"))
+//            {
+//                help_logged_in();
+//            }
+//        }
+//    }
+//
+//    public static void help_logged_in()
+//    {
+//        System.out.println("What would you like to do?");
+//        System.out.println("1. [L]og Out");
+//        System.out.println("2. [C]reate Game");
+//        System.out.println("3. [Li]st Games");
+//        System.out.println("4. [P]lay Game");
+//        System.out.println("5. [O]bserve Game");
+//        String userInput = SCANNER.nextLine().trim();
+//
+//        if (userInput.equalsIgnoreCase("log out") || userInput.equalsIgnoreCase("l"))
+//        {
+//            //Calls the server logout API to logout user
+//            //logout request?
+//            loggedIn = false;
+//            //call main to go back to the beginning
+//        }
+//
+//        else if (userInput.equalsIgnoreCase("create game") || userInput.equalsIgnoreCase("c"))
+//        {
+//            System.out.println("New game name: ");
+//            String new_game_name = SCANNER.nextLine().trim();
+//            //create game request?
+//        }
+//
+//        else if (userInput.equalsIgnoreCase("list games") || userInput.equalsIgnoreCase("li"))
+//        {
+//            //list games()
+//        }
+//
+//        else if (userInput.equalsIgnoreCase("play game") || userInput.equalsIgnoreCase("p"))
+//        {
+//            System.out.println("Which game would you like to join?: ");
+//            String game_id = SCANNER.nextLine().trim();
+//            System.out.println("Which color would you like to be? [W]hite or [B]lack?");
+//            String color = SCANNER.nextLine().trim();
+//            if (color.equalsIgnoreCase("w")) { color = String.valueOf(ChessGame.TeamColor.WHITE); }
+//            else {color = String.valueOf(ChessGame.TeamColor.BLACK);}
+//
+//            //join game()
+//        }
+//
+//        else if (userInput.equalsIgnoreCase("observe game") || userInput.equalsIgnoreCase("o"))
+//        {
+//            System.out.println("Which game would you like to observe?: ");
+//            String game_id = SCANNER.nextLine().trim();
+//            //observe games()
+//        }
+//
+//        else
+//        {
+//            System.out.println("Invalid input.");
+//            help_logged_in();
+//        }
     }
 }
